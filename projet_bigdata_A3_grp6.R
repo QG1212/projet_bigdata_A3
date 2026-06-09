5e7f845055c3dc63ffebdc5434d8fab24ff350dd
hectordevilleroche-source <hectordevilleroche@gmail.com>
2026-06-09 08:34:33 +0200

    Update projet_bigdata_A3_grp6.R
    délimitation de la france

--- projet_bigdata_A3_grp6.R
+++ projet_bigdata_A3_grp6.R
@@ -12,7 +12,6 @@ for (col in colnames(data)) {
   df[[col]][x %in% mots_inconnus] <- NA
 }
 
-# ── 2. NORMALISER les booléens en minuscules (false/true)
 bool_cols <- c("prise_type_ef", "prise_type_2", "gratuit", "reservation")
 
 for (col in bool_cols) {
@@ -23,3 +22,29 @@ for (col in bool_cols) {
     .default = NA
   )
 }
+
+library(dplyr)
+
+data <- read.csv("C:/Users/hecto/Music/IRVE (1).csv", sep = ",", stringsAsFactors = FALSE)
+
+# 2. Filtrage intelligent par "Blocs Géographiques" (Métropole + Corse)
+data_metropole_propre <- data %>%
+  mutate(
+    consolidated_latitude = as.numeric(consolidated_latitude),
+    consolidated_longitude = as.numeric(consolidated_longitude)
+  ) %>%
+  filter(
+    # ZONE 1 : Cœur et Ouest de la France
+    (consolidated_latitude >= 42.3 & consolidated_latitude <= 51.1 & 
+       consolidated_longitude >= -5.2 & consolidated_longitude <= 6.0) |
+      
+      # ZONE 2 : Est de la France (Alsace / Alpes)
+      (consolidated_latitude >= 44.5 & consolidated_latitude <= 49.5 & 
+         consolidated_longitude > 6.0 & consolidated_longitude <= 7.5) |
+      
+      # ZONE 3 : Corse
+      (consolidated_latitude >= 41.3 & consolidated_latitude <= 43.1 & 
+         consolidated_longitude >= 8.5 & consolidated_longitude <= 9.6)
+  )
+
+View(data_metropole_propre)
